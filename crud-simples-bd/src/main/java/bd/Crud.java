b8aee0cd340c20c08dc88f7f6941573b02495f33
Giovanna <giovanna.mafra@dock.tech>
2025-03-06 10:53:21 -0300

    subindo o crud simples

---
+++ crud-simples-bd/src/main/java/bd/Crud.java
@@ -0,0 +1,85 @@
+package bd;
+
+import java.sql.*;
+import java.util.Scanner;
+
+
+
+
+public class Crud {
+    static final String DB_URL = "jdbc:mysql://localhost:3306/crud";
+    static final String USER = "giovanna";
+    static final String PASS = "PercyJackson15@12";
+    static Connection conn = null;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        try{
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            System.out.println("Conectado ao banco de dados!");
+            boolean runnning = true;
+            while (running) {
+                System.out.println("1. Inserir | 2. Consultar | 3.Atualizar | 4.Deletar | 5. Sair");
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1 -> insert();
+                    case 2 -> read();
+                    case 3 -> update();
+                    case 1 -> delete();
+                    case 1 -> running = false;
+                    default -> System.out.println("Opção inválida!");
+
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try{if (conn != null) conn.close(); } catch (SQLException e) {e.printStackTrace(); }
+        }
+    }
+
+    static void insert() throws SQLException {
+        System.out.println("Nome: ");
+        String nome = scanner.next();
+        String sql = "INSERT INTO pessoa (nome) VALUES (?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1,nome);
+            stmt.executeUpdate();
+            System.out.println("Inserido com sucesso!");
+        }
+    }
+}
+
+    static void read() throws SQLException {
+      String sql = "SELECT * FROM pessoa";
+      try(Statement mtmt = conn.createStatement(); ResultSet rs = stmt.executeQuerry(sql)) {
+          whille (rs.next()) {
+              System.out.println("ID: " + rs.getInt("id") + ",Nome: " + rs.getString("nome"));
+          }
+      }
+    }
+
+    static void update() throws SQLException {
+        System.out.println("ID para atualizar: ");
+        int id = scanner.nextInt();
+        System.out.println("Novo nome: ");
+        String nome = scanner.next();
+        String sql = "UPDATE pessoa SET nome = ? WHERE id= ?";
+        try(PreparedStatement stmt = conn.preparedStatement(sql)) {
+            stmt.setString(1,nome);
+            stmt.setInt(2, id);
+            stmt.executeUpdate();
+            System.out.println("Atualizado com sucesso!");
+        }
+    }
+
+    static void delete() throws SQLException{
+        System.out.println("ID para deletar: ")
+        int id = scanner.nextInt();
+        String sql = "DELETE FROM pessoa WHERE id = ?";
+        try(PreparedStatement stmt = conn.preparedStatement(sql)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+            System.out.println("Deletado com sucesso");
+        }
+    }
---
+++ crud-simples-bd/src/main/java/bd/Main.java
@@ -0,0 +1,6 @@
+package bd;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
